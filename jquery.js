73fcba0973c6b8467a30e3067b502b78bb15de20
rhubbard09 <rhubbard09@gmail.com>
2014-01-03 14:53:48 -0800

    work on homepage

--- jquery.js
+++ jquery.js
@@ -1,1 +1,28 @@
 // JavaScript Document
+
+
+$(document).ready(function () {
+	// deals with resizing and changing divs for video
+	$('.whatwedo').mouseenter(function() {
+		$(this).css('font-size', '105%');
+	})
+	$('.whatwedo').mouseleave(function() {
+		$(this).css('font-size', '100%');
+	})
+	
+	$('.wedovideo').hide(0);
+	
+	$('.whatwedo').click(function () {
+		$(this).fadeOut(0);
+		$('.wedovideo').fadeIn(400);
+		$(window).scrollTop($('.wedovideo').offset().top);
+		$('.footerA').addClass('footerB').removeClass('footerA');
+	})
+	
+	$('.wedovideo').click(function () {
+		$(this).fadeOut(0);
+		$('.whatwedo').fadeIn(400);
+		$('.footerB').addClass('footerA').removeClass('footerB');
+	})	
+})
+
